5dec8b2565035aca0ea76035b1101de9b5a479c3
Jogi <jukaeser>
2019-05-21 11:22:31 +0200

    revised code

--- Descriptiv_Analysis/Test.py
+++ Descriptiv_Analysis/Test.py
@@ -1,8 +1,9 @@
+import matplotlib
+matplotlib.use('TkAgg')
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import matplotlib
-matplotlib.use('TkAgg')
+
 
 data = pd.read_csv('train.csv')
 
@@ -10,9 +11,9 @@ df = pd.DataFrame(data)
 df1 = df.iloc[:72:, ]
 
 df1_np = np.array(df1)
-print(df1_np.shape)
+print("Shape of the reduced Dataset:", df1_np.shape)
 
-print(df1.head())
+print("\n", df1.head())
 
 hist = df1.hist()
 plt.show()
