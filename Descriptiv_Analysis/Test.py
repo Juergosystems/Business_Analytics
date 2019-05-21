4188babbcedfdc33d458d53c96dff1d9e1958103
Jogi <jukaeser>
2019-05-21 11:20:12 +0200

    revised code

--- Descriptiv_Analysis/Test.py
+++ Descriptiv_Analysis/Test.py
@@ -1,13 +1,13 @@
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
 
 data = pd.read_csv('train.csv')
 
 df = pd.DataFrame(data)
-df1 = df.iloc[:72 :,]
+df1 = df.iloc[:72:, ]
 
 df1_np = np.array(df1)
 print(df1_np.shape)
