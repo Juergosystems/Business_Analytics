06355cd6eed9b9501aec09efaf3b2f2ff88c8b07
Jogi <jukaeser>
2019-05-21 11:30:28 +0200

    formatted example

--- Descriptiv_Analysis/Test.py
+++ Descriptiv_Analysis/Test.py
@@ -5,15 +5,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+### Read in the Datafiles
+#############################################################################################
 data = pd.read_csv('train.csv')
-
 df = pd.DataFrame(data)
-df1 = df.iloc[:72:, ]
 
+### Only take the first 100 rows of the Datafile
+#############################################################################################
+df1 = df.iloc[:100:, ]
+print(df1.head(), "\n")
+
+### Change the pandas Dataframe to a NumpyAarray to check the shape of the reduced Dataset
+#############################################################################################
 df1_np = np.array(df1)
 print("Shape of the reduced Dataset:", df1_np.shape)
 
-print("\n", df1.head())
-
+### Plot a Histogramm of the reduced Dataset
+#############################################################################################
 hist = df1.hist()
 plt.show()
