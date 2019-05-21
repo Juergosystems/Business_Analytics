bda69dd885c1da029ec659a10ce7ad1ad9d70e14
Jogi <jukaeser>
2019-05-21 08:57:22 +0200

    histogramm test

--- Descriptiv_Analysis/Test.py
+++ Descriptiv_Analysis/Test.py
@@ -1,12 +1,10 @@
 import numpy as np
-import matplotlib as mlt
 import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv('train.csv')
 
-df=pd.DataFrame(data)
+df = pd.DataFrame(data)
+df = df['Date']
+df.hist(bins=5)
 
-plt.plot(x='Store', y='Weekly_Sales')
-
-plt.show() # tested
