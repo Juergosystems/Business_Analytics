c80f3267b8033710d9ac9f924e7f39ac8351828f
Jogi <jukaeser>
2019-05-21 10:26:43 +0200

    histogramm test

--- Descriptiv_Analysis/Test.py
+++ Descriptiv_Analysis/Test.py
@@ -1,10 +1,15 @@
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv('train.csv')
 
 df = pd.DataFrame(data)
-df = df['Date']
-df.hist(bins=5)
+df1 = df.iloc[:, :72]
 
+print(df1.head())
+
+hist = df1.hist()
+plt.show()
