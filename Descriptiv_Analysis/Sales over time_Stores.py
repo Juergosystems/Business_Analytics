ed50a4cb14538aac985676f0a7ea2febee5cffd8
annasaur <anna.m.saur@gmail.com>
2019-06-06 17:05:19 +0200

    Sales Over time Plot

--- Descriptiv_Analysis/Sales over time_Stores.py
+++ Descriptiv_Analysis/Sales over time_Stores.py
@@ -2,14 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('train.csv')
-df = df.sort_values('Date', ascending=True)
-
-# create dataframe with data for store 1 only
-df_store1 = df[df.Store == 1]
+df_train = pd.read_csv('train.csv')
+df_train = df_train.sort_values('Date', ascending=True)
 
 # plot graph with grouping
-df_store1.groupby(['Date', 'Dept']).sum()['Weekly_Sales'].unstack().plot()
+df_train.groupby(['Date', 'Store']).sum()['Weekly_Sales'].unstack().plot(linewidth = 0.7)
 
 ax = plt.subplot(111)
 
@@ -20,6 +17,6 @@ ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 # Put a legend to the right of the current axis
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=6, ncol=3)
 plt.xticks(fontsize=6, rotation = 45)
-plt.title('Weekly Sales per Department, Store 1')
+plt.title('Weekly sales per store')
 
 plt.show()
